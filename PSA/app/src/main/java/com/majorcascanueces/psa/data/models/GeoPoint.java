9c5e06aa46292a3b392e15ee8f9a8c3039ecdd54
Max Saavedra <max.saavedra@unmsm.edu.pe>
2023-08-03 02:45:26 -0500

    JSON de nodos implementado

---
+++ PSA/app/src/main/java/com/majorcascanueces/psa/data/models/GeoPoint.java
@@ -0,0 +1,12 @@
+package com.majorcascanueces.psa.data.models;
+
+public class GeoPoint {
+
+    String label;
+    double longitud;
+    double latitud;
+
+    public GeoPoint(String label, double longitud, double latitud) {
+
+    }
+}
